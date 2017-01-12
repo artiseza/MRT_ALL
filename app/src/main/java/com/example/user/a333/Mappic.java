d275ae60355e91cb28ba820051eafeb8f11ed5cc
artiseza <alanlin919@gmail.com>
2017-01-13 07:47:08 +0800

    add fragment1

---
+++ app/src/main/java/com/example/user/a333/Mappic.java
@@ -0,0 +1,42 @@
+package com.example.user.a333;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Mappic extends Fragment {
+
+    private int idx = 0;
+
+    public static Mappic newInstance(String param1, String param2) {
+        Mappic fragment = new Mappic();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            idx = (int) getArguments().get("idx");
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.map_picture, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+}
